86146afc2fc90b2d634dd6e93b46edba6ab7e4cf
sathyakrishna384@gmail.com <sathyakrishna384@gmail.com>
2025-12-19 10:33:04 +0530

    fifth commit

--- tests/ClientTestPo.spec.js
+++ tests/ClientTestPo.spec.js
@@ -2,19 +2,20 @@ const{test, expect} = require('@playwright/test');
 const{POManager} = require('../PageObjectModel/POManager');
 const testData = JSON.parse(JSON.stringify(require("../utils/testData.json")));
 
-test.only("Practice Test Case", async ({page})=>{
-    let orderId;
+for(const data of testData){
+    test.only("Practice Test Case"+data.itemName, async ({page})=>{
+        let orderId;
 
-    const poManager = new POManager(page, expect);
-    await poManager.getLoginPage().goto();
-    await poManager.getLoginPage().login(testData.email, testData.password);
-    await poManager.getDashboardPage().addToCart(testData.itemName);
-    await poManager.getPaymentPage().gotoCheckoutPage();
-    await poManager.getPaymentPage().validateOrderDetails(testData.itemName, testData.email);
-    await poManager.getPaymentPage().fillDetails(testData.desiredCountry);
-    await poManager.getPaymentPage().placeOrder();
-    await poManager.getOrderSummarypage().verifyOrderDetailsInSummary(testData.itemName);
-    orderId = await poManager.getOrderSummarypage().collectOrdId();
-    await poManager.getHistoryPage().verifyOrderIdInOrderHistory(orderId);
-}   
-);
+        const poManager = new POManager(page, expect);
+        await poManager.getLoginPage().goto();
+        await poManager.getLoginPage().login(data.email, data.password);
+        await poManager.getDashboardPage().addToCart(data.itemName);
+        await poManager.getPaymentPage().gotoCheckoutPage();
+        await poManager.getPaymentPage().validateOrderDetails(data.itemName, data.email);
+        await poManager.getPaymentPage().fillDetails(data.desiredCountry);
+        await poManager.getPaymentPage().placeOrder();
+        await poManager.getOrderSummarypage().verifyOrderDetailsInSummary(data.itemName);
+        orderId = await poManager.getOrderSummarypage().collectOrdId();
+        await poManager.getHistoryPage().verifyOrderIdInOrderHistory(orderId);
+    });
+}
